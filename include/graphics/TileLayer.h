366084442b72f367262a0dac42fcd500b03ce858
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-13 19:50:09 +0200

    Update TileLayer to shift from polymorphism and hard coded tilemaps.

--- include/graphics/TileLayer.h
+++ include/graphics/TileLayer.h
@@ -1,6 +1,7 @@
 #ifndef MODWIN_ENGINE_TILELAYER_H
 #define MODWIN_ENGINE_TILELAYER_H
 
+#include <cstdint>
 #include <graphics/SurfaceLayer.h>
 #include <vector>
 #include <string>
@@ -22,26 +23,37 @@ namespace Modwin
 	//TileSetVec = TileSetList
 	using TilesetVec = std::vector<Tileset>;
 	// TileVec2D = TileMap
+	using TileGid = std::uint32_t;
 	using TileVec2D = std::vector<std::vector<int>>;
+
+	constexpr TileGid FlipHorizontal = 0x80000000;
+	constexpr TileGid FlipVertical   = 0x40000000;
+	constexpr TileGid FlipDiagonal   = 0x20000000;
+	constexpr TileGid TileIdMask     = 0x1FFFFFFF;
 
 	class TileLayer : public SurfaceLayer
 	{
 	public:
+			TileLayer(int tileWidth, int rowCount, int columnCount, TileVec2D tileVec, TilesetVec tileSetVec);
+			void Render() override;
+			void Update() override;
 
-		TileLayer(int tileWidth, int rowCount, int columnCount, TileVec2D tileVec, TilesetVec tileSetVec);
+			bool IsInBounds(int column, int row) const noexcept;
+			int GetWidth() const noexcept;
+			int GetHeight() const noexcept;
+			const std::string& GetName() const noexcept;
 
-		void Render() override;
-		void Update() override;
-
-		int CalculateTileForCorruption(int row, int column);
-		bool ContainsTile(int row, int tileId);
-		const TileVec2D& GetTileVec() const noexcept { return m_TileVec2D; }
-		const TilesetVec& GetTileSetVec() const noexcept { return m_Tilesets; }
+			int CalculateTileForCorruption(int row, int column);
+			bool ContainsTile(int row, int tileId);
+			const TileVec2D& GetTileVec() const noexcept { return m_TileVec2D; }
+			const TilesetVec& GetTileSetVec() const noexcept { return m_Tilesets; }
 	private:
-		int m_TileWidth;
-		int m_ColumnCount, m_RowCount;
-		TileVec2D m_TileVec2D;
-		TilesetVec m_Tilesets;
+
+			int m_Width, m_Height = 0;
+			std::vector<TileGid> m_Tiles;
+			int m_ColumnCount, m_RowCount;
+			TileVec2D m_TileVec2D;
+			TilesetVec m_Tilesets;
 	};
 
 } // Modwin
