966a1d5ce73446ebdac38490a6cefec22817f949
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-18 20:41:34 +0200

    Added dynamic project context scanner for savable/editable maps.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include "core/Engine.h"
+#include "core/ResourcePaths.h"
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <utility>
+
 int main(int argc, char** argv)
 {
-	(void)argc;
-	(void)argv;
+	Modwin::ProjectContext projectContext = argc > 1
+		? Modwin::CreateProjectContext(argv[1])
+		: Modwin::CreateDefaultProjectContext();
 
 	auto* engine = Modwin::Engine::GetInstance();
-	if (!engine->Init())
+	if (!engine->Init(std::move(projectContext)))
 	{
 		SDL_Quit();
 		return 1;
