d14e7b35fb8c434bf90ebc8705b999b129855b4b
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-19 17:20:19 +0200

    Updated ResourcePaths struct to correctly infer project/asset root irrespective of setup.

--- src/core/ResourcePaths.cpp
+++ src/core/ResourcePaths.cpp
@@ -1,6 +1,9 @@
 #include "core/ResourcePaths.h"
 #include <SDL3/SDL_filesystem.h>
 
+#include <system_error>
+#include <utility>
+
 namespace Modwin
 {
 	std::filesystem::path GetResourcePath(const std::filesystem::path& relativePath)
@@ -12,5 +15,33 @@ namespace Modwin
 		}
 
 		return std::filesystem::path(basePath) / "resources" / relativePath;
+	}
+
+	ProjectContext CreateDefaultProjectContext()
+	{
+		ProjectContext context;
+		context.assetRoot = GetResourcePath({}).lexically_normal();
+		context.projectRoot = context.assetRoot.parent_path();
+		context.activeMapPath = context.assetRoot / "maps" / "map.tmx";
+		return context;
+	}
+
+	ProjectContext CreateProjectContext(std::filesystem::path activeMapPath)
+	{
+		std::error_code error;
+		auto normalizedMapPath = std::filesystem::absolute(activeMapPath, error);
+		if (error)
+		{
+			normalizedMapPath = std::move(activeMapPath);
+		}
+
+		ProjectContext context;
+		context.activeMapPath = normalizedMapPath.lexically_normal();
+		context.assetRoot = context.activeMapPath.parent_path().parent_path();
+		context.projectRoot =
+			context.assetRoot.filename() == "resources"
+				? context.assetRoot.parent_path()
+				: context.assetRoot;
+		return context;
 	}
 }
