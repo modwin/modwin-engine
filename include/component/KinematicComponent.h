8bae45d0104708678bd51752fb5349db5bd203c5
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-21 19:01:37 +0200

    Revamped speed physics for NPC/Player.

--- include/component/KinematicComponent.h
+++ include/component/KinematicComponent.h
@@ -4,11 +4,18 @@
 
 #ifndef MODWIN_ENGINE_KINEMATICCOMPONENT_H
 #define MODWIN_ENGINE_KINEMATICCOMPONENT_H
+#include "physics/Vector2D.h"
 
-
-class KinematicComponent
+namespace Modwin
 {
-};
+	struct KinematicBodyComponent
+	{
+		Vector2D velocity;
+		float maximumSpeed = 180.0F;
+		float acceleration = 1200.0F;
+		float deceleration = 1600.0F;
+	};
+}
 
 
 #endif //MODWIN_ENGINE_KINEMATICCOMPONENT_H
